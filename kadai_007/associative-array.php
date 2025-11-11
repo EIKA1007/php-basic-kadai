0a790e2c17b3a8da85dfec83f17deeddabf5d5cd
EIKA1007 <eeekachan1oo7@gmail.com>
2025-11-11 10:49:03 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎7章課題</title>
+</head>
+<body>
+    <p>
+<?php
+$shop_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+// print_r関数を使いブラウザに連想配列を出力します
+print_r($shop_date);
+?>
+ </p>
+</body>
+</html>
